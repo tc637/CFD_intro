a48f9aee93eb2d644a6fb0a195b972de8c4ad8c2
tc637 <tcyc94@hotmail.com>
2016-12-01 11:40:29 -0800

    interim commit for new problem

--- mech510_p3/py_v5.py
+++ mech510_p3/py_v5.py
@@ -7,20 +7,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def read_files(meshsize, xmax, ymax):
+def read_files(meshsize,jmax,xmax,ymax,tmax,scheme,p):
     fname = ("Tmesh_cn" + str(meshsize) + "_xmax_" + str(xmax) + "_ymax_" 
-             + str(ymax) + "_t_" + "0" + "_scheme_" + "2" + ".txt")
-    afname = ("Tmesh_cn" + str(meshsize) + "_xmax_" + str(xmax) + "_ymax_"  
-             + str(ymax) + "_t_" + "1" + "_scheme_" + "2" + ".txt")
+             + str(ymax) + "_t_" + str(tmax) + "_scheme_" + str(scheme) + "_p_" + str(p) + ".txt")
 
-    asolution = []
-    with open(afname, 'r') as afile:
-        for line in afile:
-            num_list = [float(num) for num in line.split()]
-            asolution.append(num_list)
-            
-    asolution = np.array(asolution)
-    
     csolution = []
     with open(fname, 'r') as cfile:
         for line in cfile:
@@ -28,12 +18,12 @@ def read_files(meshsize, xmax, ymax):
             csolution.append(num_list)
             
     csolution = np.array(csolution)
-    sol_error = asolution - csolution
     
     dx = float(xmax)/float(meshsize)
+    dy = float(ymax)/float(jmax)
     xpos = np.array([dx*(i+0.5) for i in range(0,meshsize)])
-    
-    return(xpos,sol_error,asolution,csolution)
+    ypos = np.array([dy*(j+0.5) for j in range(0,jmax)])
+    return(xpos,ypos,csolution)
     
     
 
@@ -66,10 +56,14 @@ if __name__ == '__main__':
     plt.style.use('ggplot')
     
      
-    xmax = 1
+    xmax = 5
     ymax = 1
-
-    meshsizes = [10,20,40,80,160,320,640]
+    tmax = 0
+    scheme = 2
+    p = 2
+    jmax = 10
+    
+    meshsizes = [25,200]
     ind = 0
 
     fs = 20.
@@ -89,15 +83,14 @@ if __name__ == '__main__':
     read_analytic("analytic.dat",ana)
     read_analytic("before.dat",before)
     
-    
-    cols = np.linspace(0,1,27)
-    rows = np.linspace(0,1,12)
+    cols = np.linspace(0,1,26)
+    rows = np.linspace(0,1,11)
     
     cols_plot = (cols + np.diff(cols)[0]/2)[:-1]*5
     rows_plot = (rows + np.diff(rows)[0]/2)[:-1]
     
-    before = before[1:12,1:27]
-    ana = ana[1:12,1:27]
+    before = before[1:11,1:26]
+    ana = ana[1:11,1:26]
     
     fig1,ax1 = plt.subplots(1,1,figsize=(16,12))
     levels = np.linspace(np.min(before),np.max(before),20)
@@ -112,11 +105,11 @@ if __name__ == '__main__':
     ax1.tick_params(axis='both',which='major',labelsize=20)
     cbar.set_label(r'$\overline{T}$',size=fs+5)
     cbar.ax.tick_params(labelsize=20) 
-    """
+
     fig2,ax2 = plt.subplots(1,1,figsize=(16,12))
     levels = np.linspace(np.min(ana),np.max(ana),20)
     CS = ax2.contour(cols_plot,rows_plot,ana, levels, colors='k')
-    ax1.clabel(CS, colors='k', fmt='%6.4f', fontsize=16)
+    ax2.clabel(CS, colors='k', fmt='%6.4f', fontsize=16)
     contour_filled = ax2.contourf(cols_plot,rows_plot,ana, levels)
     cbar = plt.colorbar(contour_filled)
     ax2.set_xlabel('x', fontsize=fs, fontweight=fw)
@@ -126,20 +119,60 @@ if __name__ == '__main__':
     ax2.tick_params(axis='both',which='major',labelsize=20)
     cbar.set_label(r'$\overline{T}$',size=fs+5)
     cbar.ax.tick_params(labelsize=20) 
-    """
-    fig2,ax2 = plt.subplots(1,1,figsize=(16,12))
-    levels = np.linspace(np.min(ana),np.max(ana),20)
-    CS = ax2.contour(ana, levels, colors='k')
-    ax1.clabel(CS, colors='k', fmt='%6.4f', fontsize=16)
-    contour_filled = ax2.contourf(ana, levels)
+    
+    
+    # ========================
+    
+    
+    xpos,ypos,csol = read_files(meshsizes[ind],jmax,xmax,ymax,tmax,scheme,p)
+    
+    
+    
+    fig3,ax3 = plt.subplots(1,1,figsize=(16,12))
+    levels = np.linspace(np.min(csol),np.max(csol),20)
+    CS = ax3.contour(xpos,ypos,csol, levels, colors='k')
+    ax3.clabel(CS, colors='k', fmt='%6.4f', fontsize=16)
+    contour_filled = ax3.contourf(xpos,ypos,csol, levels)
     cbar = plt.colorbar(contour_filled)
-    ax2.set_xlabel('x', fontsize=fs, fontweight=fw)
-    ax2.set_ylabel('y', fontsize=fs, fontweight=fw)
-    ax2.set_title('Contour Plot of the Solution Mesh for the Energy Equation, Meshsize = {}, t = 0'.format(meshsizes[ind]),
+    ax3.set_xlabel('x', fontsize=fs, fontweight=fw)
+    ax3.set_ylabel('y', fontsize=fs, fontweight=fw)
+    ax3.set_title('Contour Plot of the Solution Mesh for the Energy Equation, Meshsize = {}, t = 0'.format(meshsizes[ind]),
                   fontsize=fs,fontweight=fw)
-    ax2.tick_params(axis='both',which='major',labelsize=20)
+    ax3.tick_params(axis='both',which='major',labelsize=20)
     cbar.set_label(r'$\overline{T}$',size=fs+5)
     cbar.ax.tick_params(labelsize=20) 
     
     
+    ana_wall = ana[:,0]
+    fig4,ax4=plt.subplots(1,1)
+    ax4.plot(ana_wall)
+    
+    computed_wall = [0.20482879493674047,0.50441279748591161,0.69059679858669065,0.81781279893430447,0.92234879899224020,1.0223487989922411,1.1178127989343045,1.1905967985866905,1.2044127974859113,1.1048287949367397]
    
+    ax4.plot(computed_wall)
+    computed_wall = [0.21249274965409554,
+  0.50905274923567090,
+  0.69296874905703576,
+  0.81867274900231868,
+  0.92245274899427210,
+   1.0224527489942721,
+   1.1186727490023187,
+   1.1929687490570358,
+   1.2090527492356709,
+   1.1124927496540955]
+   
+
+    ax4.plot(computed_wall)
+    
+
+   
+   
+   
+   
+   
+   
+   
+  
+  
+
+    
